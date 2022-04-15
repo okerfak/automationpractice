ec7da5fa00f42eab208fd5fb4d9011404fc9abd4
Ilya K <ilaklimcuk341@gmail.com>
2022-04-15 14:15:25 +0300

    finich the logout spec

--- cypress/integration/apitest.spec.js
+++ cypress/integration/apitest.spec.js
@@ -8,15 +8,15 @@ describe('Test suit',() => {
         cy.get('#passwd').type('245175Aq');
         cy.get('#SubmitLogin > span').click();
         // cy.intercept("GET",'/index.php?controller=authentication&back=my-account').as('logoutReq')
-        cy.request('http://automationpractice.com/index.php?controller=authentication&back=my-account')
+        cy.request('http://automationpractice.com/index.php?mylogout=')
         .then( response => {
             expect(response.status).to.eq(200)
-            expect(response.allRequestResponses[0]).to.have.property('Request URL')
-            // cy.wrap(response.allRequestResponses[0].RequestURL).as('logoutUrl') wrap void
+            expect(response.allRequestResponses[0]).to.have.property('Request URL').to.not.be.oneOf([null, ""])
+            cy.wrap(response.allRequestResponses[0].["Request URL"]).as('logoutUrl')
         })
-        // cy.get('@logoutUrl')
-        // .then(url=>{
-    //         cy.visit(url)
-    //     })
+        cy.get('@logoutUrl')
+        .then(url=>{
+            cy.visit(url)
+        })
     })
 })
